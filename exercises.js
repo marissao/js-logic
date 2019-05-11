8736a27426823f4295854027a3516c4bf47e9edf
marissao <okazaki.marissa@gmail.com>
2019-05-10 21:45:13 -1000

    practice

--- exercises.js
+++ exercises.js
@@ -11,6 +11,8 @@ if(today === "Friday"){
 If/else statements = Evaluates (or checks) a condition. If the condition is true, the first code block is executed. If the condition is false, the second code block is executed instead.
 */
 
+console.log('test')
+
 /*if(today === "Friday"){
   return "Let's Party!";
 }else{
